e84bcd79b4630d9abd00a5feb9d1477b0b019742
rishi-raj-jain <rishi18304@iiitd.ac.in>
2021-08-06 16:50:44 +0530

    Add Prefetching of the Prev/Next Posts

--- components/blog/more-posts.js
+++ components/blog/more-posts.js
@@ -1,15 +1,18 @@
 import Link from 'next/link'
+import Prefetch from '@layer0/react/Prefetch'
 
 const MorePosts = ({ morePosts }) => {
   return (
     morePosts.length > 0 && (
-      <div className="mt-10 pt-10 w-full border-t dark:border-gray-500">
+      <div className="mt-10 pt-10 w-full border-t dark:border-gray-500 flex flex-row items-center justify-between">
         {morePosts.map((item) => (
-          <Link href={`/blog/${item.slug}`} key={item.slug}>
-            <a className="border rounded p-5 flex flex-col space-y-3">
-              <span>Up Next &rarr;</span>
-              <span className="text-md font-bold">{item.content.title}</span>
-            </a>
+          <Link href={`/blog/${item.slug}`} key={item.slug} passHref>
+            <Prefetch>
+              <a className="border rounded p-5 flex flex-col space-y-3">
+                <span>{item.indicator}</span>
+                <span className="text-md font-bold">{item.name}</span>
+              </a>
+            </Prefetch>
           </Link>
         ))}
       </div>
